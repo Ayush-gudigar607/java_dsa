b0695bb943450f4d739fabc4b88b34c165bd3806
unknown <23ai009@sitmng.ac.in>
2026-07-25 17:53:18 +0530

    add the vote.java

---
+++ BASIC CONCEPTS/CONDITIONAL STATEMENT PROBLEMS/begginer/vote.java
@@ -0,0 +1,18 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Age :");
+        int age=sc.nextInt();
+        if(age>=18)
+        {
+            System.out.println("Person is eligible to vote");
+        }
+        else
+        {
+            System.out.println("person is Not Eligible to vote");
+        }
+    }
+}
