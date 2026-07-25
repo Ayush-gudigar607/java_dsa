883403b14f3039fd22a29e63ca7a7cbb507265cb
unknown <23ai009@sitmng.ac.in>
2026-07-25 17:48:54 +0530

    add the positive to begginer and evnnodd program has been feeded

---
+++ BASIC CONCEPTS/CONDITIONAL STATEMENT PROBLEMS/begginer/positive.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the   positive number:");
+        int n=sc.nextInt();
+        if(n%2==0)
+        {
+            
+            System.out.println("Even Number\n");
+        }
+        else
+        {
+         System.out.println("not a positive number");
+
+        }
+    }
+}
