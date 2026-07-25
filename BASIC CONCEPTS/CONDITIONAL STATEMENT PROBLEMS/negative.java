ca79d29acc7a117f59d63be10b4413ede30044a1
unknown <23ai009@sitmng.ac.in>
2026-07-25 17:45:36 +0530

    add the negative.java

---
+++ BASIC CONCEPTS/CONDITIONAL STATEMENT PROBLEMS/negative.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Negative number:");
+        int n=sc.nextInt();
+        if(n%2!=0)
+        {
+            
+            System.out.println("Negative Number\n");
+        }
+        else
+        {
+         System.out.println("Not a Negative number");
+
+        }
+    }
+}
