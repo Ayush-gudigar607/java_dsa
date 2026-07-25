97cd34fd4fc84b6f8495a2ec322c4dee97c983b7
unknown <23ai009@sitmng.ac.in>
2026-07-25 18:05:18 +0530

    add the largestnumber.java

---
+++ BASIC CONCEPTS/CONDITIONAL STATEMENT PROBLEMS/intermidiate/largestthree.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class largestthree {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int a=sc.nextInt();
+        System.out.println("Enter the second Number:");
+        int b=sc.nextInt();
+        System.out.println("Enter the third Number:");
+        int c=sc.nextInt();
+        if(a>b && a>c)
+        {
+            System.out.println("a is greater number");
+        }
+        else if(b>c)
+        {
+            System.out.println("b is greater number");
+        }
+        else
+        {
+            System.out.println("c is greater number");
+        }
+    }
+}
