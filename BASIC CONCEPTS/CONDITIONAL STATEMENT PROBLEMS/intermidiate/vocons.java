7acd20459fbaa106245fb771d31008c27b2a988a
unknown <23ai009@sitmng.ac.in>
2026-07-25 18:41:49 +0530

    add the file

---
+++ BASIC CONCEPTS/CONDITIONAL STATEMENT PROBLEMS/intermidiate/vocons.java
@@ -0,0 +1,36 @@
+// import java.util.*;
+// public class vocons {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         char ch=sc.next().charAt(0);
+//         if((ch =='a') || (ch =='e') || (ch =='i') || (ch =='o') || (ch =='u'))
+//         {
+//             System.out.println(ch + "is Vowel");
+//         }
+//         else
+//         {
+//             System.out.println(ch + "is consonants");
+//         }
+        
+//     }
+// }
+
+import java.util.*;
+
+public class vocons {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String ch=sc.next();
+        if(((ch.equals('a'))) || ((ch.equals('e'))) || ((ch.equals('i'))) || ((ch.equals('o'))) || ((ch.equals('u'))))
+        {
+            System.out.println(ch + "is Vowel");
+        }
+        else
+        {
+            System.out.println(ch + "is consonants");
+        }
+        
+    }
+}
+
+
