0fb967df6312c9e76cd607b78ed52e06cd7fa414
unknown <23ai009@sitmng.ac.in>
2026-07-26 15:39:09 +0530

    add the invertedpyramid.java

---
+++ BASIC CONCEPTS/loops/pattern/invertedhalf.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class invertedhalf {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n=sc.nextInt();
+        System.out.println("Enter the number of columns:");
+        int m=sc.nextInt();
+        for(int i=0;i<n;i++){
+         for (int j=1;j<=n-i;j++)
+         {
+         System.out.print("*");
+         }
+         System.out.println();
+          
+        }
+    }
+}
