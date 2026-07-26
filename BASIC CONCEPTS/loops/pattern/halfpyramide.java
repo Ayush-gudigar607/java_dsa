5984890fd9ffd3181b4f924d57e76cf7e2d006fc
unknown <23ai009@sitmng.ac.in>
2026-07-26 13:50:12 +0530

    fix the issue in halfpyramid miss of flower bracket

---
+++ BASIC CONCEPTS/loops/pattern/halfpyramide.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class halfpyramide {
+  public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n=sc.nextInt();
+        System.out.println("Enter the number of columns:");
+        int m=sc.nextInt();
+        for(int i=1;i<=n;i++){
+         for (int j=1;j<=i;j++)
+         {
+        System.out.print(" *");
+         }
+         System.out.println();
+          
+         System.out.println();          
+        }   
+}
+}
