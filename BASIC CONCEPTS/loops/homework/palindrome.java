04203ddf5e83c8fecbe8a09e2d96c72c68af9429
unknown <23ai009@sitmng.ac.in>
2026-07-26 12:28:52 +0530

    add the palindrome code

---
+++ BASIC CONCEPTS/loops/homework/palindrome.java
@@ -0,0 +1,20 @@
+    import java.util.*;
+
+public class palindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int reversed=0;
+        int temp=n;
+        int rem;
+        while(n>0)
+        {
+            reversed=reversed * 10+n % 10;
+            n=n/10;
+        }
+       System.out.println(temp==reversed);
+        
+    }
+}
+
