e18210bcc2623426483de900f075c712537539c8
unknown <23ai009@sitmng.ac.in>
2026-07-27 23:05:33 +0530

    add the product of two numbers of the file CalculateProduct.java

---
+++ BASIC CONCEPTS/function/CalculateProduct.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class CalculateProduct {
+   public static int CalculateProduct(int a,int b)
+    {
+        int product=a*b;
+        return product;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the one number:");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number:");
+        int b=sc.nextInt();
+        System.out.println("The final result is:");
+        int result=CalculateProduct(a,b);
+        System.out.println(result);
+    }  
+}
