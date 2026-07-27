1f9529c32daccb3ad64fd2ceb0b680d0c48e881f
unknown <23ai009@sitmng.ac.in>
2026-07-27 23:00:41 +0530

    add the sum.java

---
+++ BASIC CONCEPTS/function/sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class sum {
+    public static int sum(int a,int b)
+    {
+        int sum=a+b;
+        return sum;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the one number:");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number:");
+        int b=sc.nextInt();
+        System.out.println("The final result is:");
+        int result=sum(a,b);
+        System.out.println(result);
+    }
+}
