da02fa7a70978a4bf5f6e661f3cf0e99283b2c15
unknown <23ai009@sitmng.ac.in>
2026-07-28 20:49:53 +0530

    add the infinteloop.java

---
+++ BASIC CONCEPTS/function/infinite.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class infinite {
+    public static void infiniteloop() {
+    do{
+System.out.print("****");
+    }while(true);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    infiniteloop();
+
+  }
+}
