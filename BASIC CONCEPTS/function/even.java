0c3e7a35693faf936acb84f67799a497e36ab4fb
unknown <23ai009@sitmng.ac.in>
2026-07-28 19:33:15 +0530

    add the even.java

---
+++ BASIC CONCEPTS/function/even.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class even {
+
+  public static   int  even(int n) {
+    if(n%2==0)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int res = even(n);
+    // If isPrime already printed an error message, don't print prime/composite again
+
+    if (res==1) 
+    {
+      System.out.print("Even number");
+    }
+    else
+    {
+System.out.print("odd number ");
+    } 
+  }
+}
