e0143006875711972d4c0a7bb23bbb5b4d13bf3e
unknown <23ai009@sitmng.ac.in>
2026-07-29 21:16:49 +0530

    add the oddsum.java

--- BASIC CONCEPTS/function/oddsum.java
+++ BASIC CONCEPTS/function/oddsum.java
@@ -21,3 +21,33 @@ public class oddsum {
   
   }
 }
+
+// import java.util.*;
+
+// public class oddsum {
+
+//   public static  void  oddSum(int n) {
+//     int sum=0;
+//     int countOdd=1;
+//     for(int i=1;i<=n;i++)
+//     {
+//       sum+=countOdd;
+//       countOdd+=2;
+//     }
+//     System.out.print("Sum of odd numbers:"+sum);
+//   }
+
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n=sc.nextInt();
+//     oddSum(n);    
+  
+//   }
+// }
+
+
+
+
+
+
+
