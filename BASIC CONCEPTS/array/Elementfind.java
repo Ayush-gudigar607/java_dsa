be624b3c6a15c70aca8a63334f82f5106118bc70
unknown <23ai009@sitmng.ac.in>
2026-07-31 20:41:13 +0530

    add the elementfind by using numbers.length

--- BASIC CONCEPTS/array/Elementfind.java
+++ BASIC CONCEPTS/array/Elementfind.java
@@ -30,3 +30,36 @@ public class Elementfind {
 
 }
 }
+
+
+// import java.util.*;
+
+// public class arratinput {
+//     public static void main(String args[])
+//     {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter the size of the array:");
+
+//         int numbers[]=new int[5];
+//         for (int i=0;i<numbers.length;i++ )
+//         {
+//           numbers[i]=sc.nextInt();
+//         } 
+
+//         System.out.println("Enter the number want to search it from the Given array:");
+//         int searchNumber=sc.nextInt();
+
+
+
+//         for(int i=0;i<numbers.length;i++)
+//         {
+//           if(numbers[i]==searchNumber)
+//           {
+//             System.out.println("The index of the number is "+i);
+//           }
+
+//         }
+
+
+// }
+// }
