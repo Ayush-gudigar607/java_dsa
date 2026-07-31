dfcd14de66337cda53b01416ca4dbafdf5f4ebc3
unknown <23ai009@sitmng.ac.in>
2026-07-31 20:38:36 +0530

    add the findelelement.java using the size as a primary thing

---
+++ BASIC CONCEPTS/array/Elementfind.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Elementfind {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size=sc.nextInt();
+
+        int numbers[]=new int[size];
+        for (int i=0;i<size ;i++ )
+        {
+          numbers[i]=sc.nextInt();
+        } 
+
+        System.out.println("Enter the number want to search it from the Given array:");
+        int searchNumber=sc.nextInt();
+
+
+
+        for(int i=0;i<size;i++)
+        {
+          if(numbers[i]==searchNumber)
+          {
+            System.out.println("The index of the number is "+i);
+          }
+
+        }
+
+
+}
+}
