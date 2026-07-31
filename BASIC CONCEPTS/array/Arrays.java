e02a871012ce60312384c9057f2f90b01c09696b
unknown <23ai009@sitmng.ac.in>
2026-07-31 19:46:58 +0530

    add the for loop which will help  to print the statement

--- BASIC CONCEPTS/array/Arrays.java
+++ BASIC CONCEPTS/array/Arrays.java
@@ -5,10 +5,15 @@ public class Arrays{
       marks[0]=97;
       marks[1]=99;
       marks[2]=98;
-      System.out.println(marks); //this gives garbage value because it is an array and not a single variable
-      System.out.println(marks[0]);
-      System.out.println(marks[1]);
-      System.out.println(marks[2]);
+      // System.out.println(marks); //this gives garbage value because it is an array and not a single variable
+      // System.out.println(marks[0]);
+      // System.out.println(marks[1]);
+      // System.out.println(marks[2]);
+
+      for(int i=1;i<=3;i++)
+      {
+        System.out.println(marks[i]); //this gives error because array index starts from 0 and ends at n-1
+      }
 
 
     }
