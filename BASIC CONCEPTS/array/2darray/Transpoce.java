a67693a2757bad5a27d4f13c82b3be331dd15a4f
unknown <23ai009@sitmng.ac.in>
2026-08-02 22:17:33 +0530

    add the transporce.java

---
+++ BASIC CONCEPTS/array/2darray/Transpoce.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Transpoce
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+        int[][] matrix=new int[row][col];
+
+        for (int i=0;i<row ;i++ )
+        {
+            for (int j=0;j<col ;j++ ) 
+            {
+                matrix[i][j]=sc.nextInt();
+            }
+        } 
+
+
+        //output for transpoce matrix
+         for (int j=0;j<col;j++ )
+        {
+            for (int i=0;i<row ;i++ ) 
+            {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        } 
+    }
+}
