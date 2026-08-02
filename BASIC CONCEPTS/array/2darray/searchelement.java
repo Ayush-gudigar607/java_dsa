e3a7f360d4d551ef021676f7d6ae7414012d7af4
unknown <23ai009@sitmng.ac.in>
2026-08-02 18:54:01 +0530

    add the element to search

--- BASIC CONCEPTS/array/2darray/searchelement.java
+++ BASIC CONCEPTS/array/2darray/searchelement.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Twodarray {
+public class searchelement {
     
     public static void main(String args[])
     {
@@ -21,12 +21,18 @@ public class Twodarray {
             }
         }
 
+        System.out.println("Enter the element to be find:");
+        int number=sc.nextInt();
+
         System.out.println("The Entered values are:");
         for(int i=0;i<row;i++)
         {
             for(int j=0;j<col;j++)
             {
-                System.out.print(numbers[i][j] + " "); 
+              if(numbers[i][j]==number)
+              {
+                System.out.print("number found at location:"+(i)+" "+j);
+              }
             }
             System.out.println();
         }
