8d8a16db22263ad7613f0cdb0c17e736572662c6
unknown <23ai009@sitmng.ac.in>
2026-08-03 21:46:59 +0530

    add the maxtwodarray.java

---
+++ BASIC CONCEPTS/array/2darray/MaxTwodarray.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class MaxTwodarray {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the row to be entered:");
+      int row=sc.nextInt();
+      System.out.println("Enter the column to be entered:");
+      int col=sc.nextInt();
+
+      int[][] matrix=new int[row][col];
+      
+System.out.println("Enter the values:");
+      for(int i=0;i<row;i++)
+      {
+        for (int j=0;j<col ;j++ )
+        {
+          matrix[i][j]=sc.nextInt();
+        } 
+      }
+
+      int max=matrix[0][0];
+
+      //Output
+      for (int i=0;i<row ;i++ )
+      {
+        for (int j=0;j<col ;j++ )
+        {
+          if(max<matrix[i][j])
+          {
+            max=matrix[i][j];
+          }
+        }
+        System.out.println(); 
+      }
+
+      System.out.println("Maximum value to be in array:" +max); 
+    }
+}
