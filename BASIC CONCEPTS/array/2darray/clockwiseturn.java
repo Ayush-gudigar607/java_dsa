4c44bbc1283fe4ccfde8d2634be0c9ed646a31ce
unknown <23ai009@sitmng.ac.in>
2026-08-05 21:16:13 +0530

    add the clockwise.java

---
+++ BASIC CONCEPTS/array/2darray/clockwiseturn.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class clockwiseturn {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int row=sc.nextInt();
+      int col=sc.nextInt();
+
+      int[][] matrix=new int[row][col];
+      int[] primarydiagonal=new int[row];
+      int[] secondaryDiagonal=new int[row];
+
+      for(int i=0;i<row;i++)
+      {
+        for(int j=0;j<col;j++)
+        {
+          matrix[i][j]=sc.nextInt();
+        }
+      }
+
+      //keep the j as constant 
+      for(int j=0;j<col;j++)
+      {
+        for (int i=row-1;i>=0 ;i--)
+        {
+          System.out.print(matrix[i][j]+" ");
+        } 
+        System.out.println();
+      }
+    
+    }
+}
