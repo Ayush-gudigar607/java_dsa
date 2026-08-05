f507d41bb5a226a7ba7e47223607031022648f31
unknown <23ai009@sitmng.ac.in>
2026-08-05 20:04:17 +0530

    add the largest.java

---
+++ BASIC CONCEPTS/array/2darray/largest.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class largest {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int row=sc.nextInt();
+      int col=sc.nextInt();
+
+      int[][] matrix=new int[row][col];
+
+      for(int i=0;i<row;i++)
+      {
+        for(int j=0;j<col;j++)
+        {
+          matrix[i][j]=sc.nextInt();
+        }
+      }
+      
+int largest=matrix[0][0];
+       for(int i=0;i<row;i++)
+      {
+        for(int j=0;j<col;j++)
+        {
+          if(largest<matrix[i][j])
+          {
+            largest=matrix[i][j];
+          }
+        }
+      }
+
+      System.out.println("largest in the matrix is:"+largest);
+
+      
+    }
+}
