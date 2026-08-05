9adde67be7e818b46f9a5bcd1958727bec98c28f
unknown <23ai009@sitmng.ac.in>
2026-08-05 20:00:30 +0530

    add the sum.java

---
+++ BASIC CONCEPTS/array/2darray/sum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int row=sc.nextInt();
+      int col=sc.nextInt();
+      int sum=0;
+
+      int[][] matrix=new int[row][col];
+
+      for(int i=0;i<row;i++)
+      {
+        for(int j=0;j<col;j++)
+        {
+          matrix[i][j]=sc.nextInt();
+        }
+      }
+
+       for(int i=0;i<row;i++)
+      {
+        for(int j=0;j<col;j++)
+        {
+          sum+=matrix[i][j];
+        }
+      }
+
+      System.out.println("Sum of the matrix is:"+sum);
+
+      
+    }
+}
