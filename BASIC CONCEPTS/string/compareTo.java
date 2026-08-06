8723dedea204b8ae6a174d6a28079449f338b7fa
unknown <23ai009@sitmng.ac.in>
2026-08-06 22:16:25 +0530

    add the compareTo.java

---
+++ BASIC CONCEPTS/string/compareTo.java
@@ -0,0 +1,16 @@
+
+
+public class compareTo {
+    public static void main(String[] args)
+    {
+      String name1="tony";
+      String name2="tony";
+      if(name1.compareTo(name2)==0)
+      {
+System.out.print("Strings are equal");
+      }
+      else{
+        System.out.print("Strings are not equal");
+      }
+    }
+}
