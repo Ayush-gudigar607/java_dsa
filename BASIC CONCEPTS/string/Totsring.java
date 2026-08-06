e8e967968cd20960e99b2afa340f4cf2718a5e5d
unknown <23ai009@sitmng.ac.in>
2026-08-06 23:01:45 +0530

    add the Tostring.java

---
+++ BASIC CONCEPTS/string/Totsring.java
@@ -0,0 +1,9 @@
+
+public class Totsring {
+    public static void main(String[] args)
+    {
+        int number=1234;
+        String str=Integer.toString(number);
+        System.out.print(str);
+    }
+}
