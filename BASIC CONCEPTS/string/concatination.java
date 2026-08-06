c4dbd0e97f23232094d2c192bef16a3953424ce6
unknown <23ai009@sitmng.ac.in>
2026-08-06 21:21:34 +0530

    add the cocatination.java

---
+++ BASIC CONCEPTS/string/concatination.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class concatination {
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     //concatination
+     String name=sc.nextLine();
+     String lastname=sc.nextLine();
+     String fullName=name+lastname;
+     System.out.print(fullName);
+    }
+}
