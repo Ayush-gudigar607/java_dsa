4aa01d4900011db5770a81cc5c30e3266a12a284
unknown <23ai009@sitmng.ac.in>
2026-08-06 22:25:59 +0530

    add the subsring.java

---
+++ BASIC CONCEPTS/string/substring.java
@@ -0,0 +1,10 @@
+
+
+public class substring {
+    public static void main(String[] args)
+    {
+      String sentence="My name is Tony";
+      String name=sentence.substring(11);
+      System.out.print(name);
+    }
+}
