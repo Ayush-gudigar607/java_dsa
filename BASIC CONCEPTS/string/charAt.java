3c5e77ed29cf28a21a679ac27fe070751e96d828
unknown <23ai009@sitmng.ac.in>
2026-08-06 21:43:33 +0530

    add the charAt.java

---
+++ BASIC CONCEPTS/string/charAt.java
@@ -0,0 +1,14 @@
+
+
+public class charAt {
+    public static void main(String[] args)
+    {
+      String firstname="Hello";
+      String lastname="Boss";
+      String fullName=firstname+lastname;
+      for (int i=0;i<fullName.length() ;i++ )
+      {
+        System.out.println(fullName.charAt(i));
+      } 
+    }
+}
