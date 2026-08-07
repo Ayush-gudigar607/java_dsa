522d37ade388e751f13bd95160931ae1dee62da3
unknown <23ai009@sitmng.ac.in>
2026-08-07 20:58:03 +0530

    add the code which calculates the length

---
+++ BASIC CONCEPTS/stringbuilder/length.java
@@ -0,0 +1,8 @@
+
+public class length {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Hello");
+        sb.length();
+        System.out.println("sb");
+    }
+}
