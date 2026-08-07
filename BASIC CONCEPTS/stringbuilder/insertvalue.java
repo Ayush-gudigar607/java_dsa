867dbafc0437b06649d003b06d3e427359c66121
unknown <23ai009@sitmng.ac.in>
2026-08-07 20:52:27 +0530

    add the code insert value

---
+++ BASIC CONCEPTS/stringbuilder/insertvalue.java
@@ -0,0 +1,8 @@
+
+public class insertvalue {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Hello");
+        sb.insert(0, "m");
+        System.out.println(sb);
+    }
+}
