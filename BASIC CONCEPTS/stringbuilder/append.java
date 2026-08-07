b881d044a78d7bbdd420589ae8c5a139cc642b91
unknown <23ai009@sitmng.ac.in>
2026-08-07 20:56:14 +0530

    add the code of append

---
+++ BASIC CONCEPTS/stringbuilder/append.java
@@ -0,0 +1,8 @@
+
+public class append {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("hell");
+        sb.append("o");
+        System.err.println(sb);
+    }
+}
