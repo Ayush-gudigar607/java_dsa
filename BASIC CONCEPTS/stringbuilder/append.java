7424543b8041656e9ad175130a839fb4322f1ccd
unknown <23ai009@sitmng.ac.in>
2026-08-07 21:01:01 +0530

    fix the values

--- BASIC CONCEPTS/stringbuilder/append.java
+++ BASIC CONCEPTS/stringbuilder/append.java
@@ -3,6 +3,8 @@ public class append {
     public static void main(String[] args) {
         StringBuilder sb=new StringBuilder("hell");
         sb.append("o");
+        sb.append("a");
+        sb.append("b");
         System.err.println(sb);
     }
 }
--- BASIC CONCEPTS/stringbuilder/declrationofstringbuilder.java
+++ BASIC CONCEPTS/stringbuilder/declrationofstringbuilder.java
@@ -2,36 +2,5 @@ public class declrationofstringbuilder{
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("Tony Stark");
         System.out.print(sb);
-
-        // Character at index 0
-        System.out.println(sb.charAt(0));
-
-        //set char at index 0
-        sb.setCharAt(0,'p');
-        System.out.println(sb);
-
-         sb.setCharAt(0,'t');
-        System.out.println(sb);
-
-        sb.insert(0,'S');
-        System.out.println(sb);
-//insert the values
-        sb.insert(3,'n');
-        System.out.println(sb);
-
-        //delete the Index(only delete index 2)
-        sb.delete(2,3);
-        System.out.print(sb);
-
-        //append 
-        sb.append("a");
-        sb.append("b");
-        System.out.print(sb);
-
-        System.out.print(sb.length());
-
-
-
-
     }
 }
