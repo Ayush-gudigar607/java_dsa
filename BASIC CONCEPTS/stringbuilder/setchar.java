0ca21ef29e1f78e352b02d78d10a9e6394932ee4
unknown <23ai009@sitmng.ac.in>
2026-08-07 20:50:18 +0530

    add the setchar.java

---
+++ BASIC CONCEPTS/stringbuilder/setchar.java
@@ -0,0 +1,9 @@
+
+public class setchar {
+    public static void main(String[] main)
+    {
+        StringBuilder sb=new StringBuilder("hello");
+        sb.setCharAt(0, 'P');
+        System.out.println(sb);
+    }
+}
