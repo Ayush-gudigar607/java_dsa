2e3c2c39e4e3889120cc9f89ac60b6a933b6213b
unknown <23ai009@sitmng.ac.in>
2026-08-07 20:54:17 +0530

    add the values

---
+++ BASIC CONCEPTS/stringbuilder/delete.java
@@ -0,0 +1,8 @@
+
+public class delete {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Hello");
+        sb.delete(0, 2);
+        System.out.println(sb);
+    }
+}
