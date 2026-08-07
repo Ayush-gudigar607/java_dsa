e35874bde5bb4b5c7ba1f6a04e614aa3b08b03c3
unknown <23ai009@sitmng.ac.in>
2026-08-07 20:47:28 +0530

    add the getchar.java

---
+++ BASIC CONCEPTS/stringbuilder/getchar.java
@@ -0,0 +1,8 @@
+
+public class getchar {
+    public static void main(String[] args)
+    {
+        StringBuilder sb=new StringBuilder("Hello");
+        System.out.println(sb.charAt(0));
+    }
+}
