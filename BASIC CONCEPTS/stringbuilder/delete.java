96ba0870245b893e052bb0e7b673be5a8d5d0479
unknown <23ai009@sitmng.ac.in>
2026-08-08 23:20:58 +0530

    add the doc to the delete

--- BASIC CONCEPTS/stringbuilder/delete.java
+++ BASIC CONCEPTS/stringbuilder/delete.java
@@ -1,4 +1,4 @@
-
+//this is the code where we can delete the string in the stringbuilder class
 public class delete {
     public static void main(String[] args) {
         StringBuilder sb=new StringBuilder("Hello");
