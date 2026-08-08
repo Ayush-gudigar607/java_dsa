9b2bbaac66cae44c1a3e99302d8e9552fef75174
unknown <23ai009@sitmng.ac.in>
2026-08-08 23:20:25 +0530

    add the doc tpo declaration

--- BASIC CONCEPTS/stringbuilder/declrationofstringbuilder.java
+++ BASIC CONCEPTS/stringbuilder/declrationofstringbuilder.java
@@ -1,3 +1,4 @@
+//this is the code where we can declare the stringbuilder class
 public class declrationofstringbuilder{
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("Tony Stark");
