2dcd3fe03bb99c52128c38849f9b2e5fccc92faa
unknown <23ai009@sitmng.ac.in>
2026-08-09 20:29:39 +0530

    add the setnumber.java

---
+++ BASIC CONCEPTS/Bit-Manipulation/setnumber.java
@@ -0,0 +1,17 @@
+//this is for set  the bit
+public class setnumber {
+    public static void main(String[] args) {
+      int n=5;
+      int pos=1;
+      int bitMask=1<<pos;
+      int newnumber=(bitMask | n);
+
+      if(newnumber==0)
+      {
+        System.out.print("bit is zero");
+      }
+      else{
+        System.out.print("bit is one");
+      }
+    }
+}
