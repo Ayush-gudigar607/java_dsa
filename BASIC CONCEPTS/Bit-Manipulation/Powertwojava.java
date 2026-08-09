c8d2942748e7524160437b2e78e8887643e1d6b7
unknown <23ai009@sitmng.ac.in>
2026-08-09 21:19:29 +0530

    add the powertwojava.java program

---
+++ BASIC CONCEPTS/Bit-Manipulation/Powertwojava.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Powertwojava {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int oper=(n & (n-1));
+        if(oper==0 && n>0)
+        {
+            System.out.print("Power of 2");
+        }
+        else{
+            System.out.print("Not a power of 2");
+        }
+        
+    }
+}
