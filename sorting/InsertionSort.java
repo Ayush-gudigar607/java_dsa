a9d880705c16694eea43982ac68ba0f7428a5d25
unknown <23ai009@sitmng.ac.in>
2026-08-13 23:12:54 +0530

    add the insertionsort.java

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,5 +1,6 @@
 //This is an implementation of insertion sort in java
 public class InsertionSort {
+  //function to print the array element
   public static void printArray(int arr[])
   {
     for (int i=0;i<arr.length ;i++ )
@@ -11,17 +12,22 @@ public class InsertionSort {
     public static void main(String[] args) {
       int arr[]={7,8,3,1,2};
 
-      //insertion sort
+      //insertion sort(time complexity is O(n^2))
       for(int i=1;i<arr.length;i++)
       {
+        //current element
         int current=arr[i];
+        //previous element
         int j=i-1;
+
         while(j>=0 && current<arr[j])
         {
+          //shift
          arr[j+1]=arr[j];
          j--;
         }
-        //placemetent
+
+        //placement
         arr[j+1]=current;
 
       }
