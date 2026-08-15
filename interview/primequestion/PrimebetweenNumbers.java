13eb4ccc2206ea35d76b509b6ffaae0bd5ee3d5d
unknown <23ai009@sitmng.ac.in>
2026-08-15 12:04:16 +0530

    added the optimized code in comment

--- interview/primequestion/PrimebetweenNumbers.java
+++ interview/primequestion/PrimebetweenNumbers.java
@@ -1,23 +1,28 @@
 
 import java.util.Scanner;
 
-public class PrimebetweenNumbers{
+public class PrimebetweenNumbers {
 
-    public static void printNumbers(int n1,int n2) {
-        int num = 2;
+    public static void printNumbers(int n1, int n2) {
+        // int start=Math.min(n1,n2);
+        // int end=Math.max(n1,n2);
 
-        for(int i=n1+1;i<n2;i++)
-        {
-          if(isPrime(i))
-          {
-            System.out.print(i+" ");
-          }
-                      num++;
+        for (int i = n1 + 1; i < n2; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+
+            // for(int i=start+1;i<end;i++)
+            // {
+            //     if(isPrime(i))
+            //     {
+            //         System.out.print(i+" ");
+            //     }
+            // }
 
         }
 
-        }
-    
+    }
 
     public static boolean isPrime(int n) {
         if (n < 2) {
@@ -36,14 +41,14 @@ public class PrimebetweenNumbers{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
-        int n2=sc.nextInt();
+        int n2 = sc.nextInt();
 
-        if (n1 == 1 && n2==1) {
+        if (n1 == 1 && n2 == 1) {
             System.out.print("2");
-        } else if (n1 == 2 && n2==1) {
+        } else if (n1 == 2 && n2 == 1) {
             System.out.print("2 3");
         } else {
-            printNumbers(n1,n2);
+            printNumbers(n1, n2);
         }
     }
 }
