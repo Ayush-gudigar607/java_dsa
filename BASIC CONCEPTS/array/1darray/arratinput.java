c1bd0de5cdcd249c7aa0256c95e06c14fab11460
unknown <23ai009@sitmng.ac.in>
2026-08-16 23:29:32 +0530

    add the doc to arrayinput

--- BASIC CONCEPTS/array/1darray/arratinput.java
+++ BASIC CONCEPTS/array/1darray/arratinput.java
@@ -1,16 +1,19 @@
+//Program to take input of an array from user
+
 import java.util.*;
 
 public class arratinput {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
 
-        int numbers[]=new int[size];
-        for(int i=0;i<size;i++)
-        {
-            System.out.println("Enter the number at index "+i);
-            numbers[i]=sc.nextInt();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int numbers[] = new int[size];
+
+        //taking input of array from user
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter the number at index " + i);
+            numbers[i] = sc.nextInt();
         }
     }
 }
