e17116f1cc33cff7297b67a830be379d7bb93274
unknown <23ai009@sitmng.ac.in>
2026-08-16 23:33:13 +0530

    add the doc to the program

--- BASIC CONCEPTS/array/1darray/duplicate.java
+++ BASIC CONCEPTS/array/1darray/duplicate.java
@@ -1,7 +1,9 @@
+//Program to remove duplicate elements from an array
 import java.util.*;
 import java.util.Arrays;
 
-public class duplicate{
+public class duplicate {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -13,19 +15,15 @@ public class duplicate{
         for (int i = 0; i < n; i++) {
             matrix[i] = sc.nextInt();
         }
+     //sorting the array and printing the unique elements using Arrays.sort() method
+        Arrays.sort(matrix);
+        System.out.print(matrix[0] + " ");
+        for (int i = 1; i < matrix.length; i++) {
 
-Arrays.sort(matrix);
-System.out.print(matrix[0]+" ");
-        for(int i=1;i<matrix.length;i++)
-        {
-          
-            if(matrix[i]!=matrix[i-1])
-            {
-              System.out.print(matrix[i]+" ");
+            if (matrix[i] != matrix[i - 1]) {
+                System.out.print(matrix[i] + " ");
             }
-            
-  
-          
+
         }
 
     }
