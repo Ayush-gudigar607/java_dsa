18fd91e8c215f0351f51eb76ae6833e247404084
unknown <23ai009@sitmng.ac.in>
2026-08-16 12:13:31 +0530

    add the function to sumofevennumbers.java

---
+++ BASIC CONCEPTS/function/SumOfEvenNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SumOfEvenNumbers {
+    public static  int SumOfEvenNumbers(int n)
+    {
+        return n*(n+1);
+    }
+
+public static void main(String[] args )
+{
+int n=new Scanner(System.in).nextInt();
+System.out.print(SumOfEvenNumbers(n));
+}
+}
