c4c689958d342207d5e0e816243adfe03862be1e
unknown <23ai009@sitmng.ac.in>
2026-08-17 11:16:25 +0530

    add the doc and spacing fix in  reverse.java

--- BASIC CONCEPTS/stringbuilder/reverse.java
+++ BASIC CONCEPTS/stringbuilder/reverse.java
@@ -1,21 +1,24 @@
 //reverse the string using the stringbuilder with front and back method
+
 public class reverse {
-    public static void main(String[] args)
-    {
-        StringBuilder sb=new StringBuilder("Tony Stark");
 
-        for(int i=0;i<sb.length()/2;i++)
-        {
-            int front=i;
-            int back=sb.length()-1-i;
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Tony Stark");
 
-            char frontChar=sb.charAt(front);
-            char backChar=sb.charAt(back);
+        for (int i = 0; i < sb.length() / 2; i++) {   //intialize the front and back index
+            int front = i;
+            int back = sb.length() - 1 - i;
 
-            sb.setCharAt(front,backChar);
+            //get the character at front and back index
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            //set the character at front and back index
+            sb.setCharAt(front, backChar);
             sb.setCharAt(back, frontChar);
 
         }
+        //print the reversed string
         System.out.println(sb);
     }
 }
