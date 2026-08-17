9a7a9a3151db8c9794f2358c44b9fd2b705eeb25
unknown <23ai009@sitmng.ac.in>
2026-08-17 12:28:42 +0530

    add the doc to the bit-manipulation

--- BASIC CONCEPTS/Bit-Manipulation/Powertwojava.java
+++ BASIC CONCEPTS/Bit-Manipulation/Powertwojava.java
@@ -4,7 +4,7 @@ public class Powertwojava {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-
+        //logic to check if the number is power of 2 or not
         int oper=(n & (n-1));
         if(oper==0 && n>0)
         {
--- BASIC CONCEPTS/Bit-Manipulation/togglebit.java
+++ BASIC CONCEPTS/Bit-Manipulation/togglebit.java
@@ -5,7 +5,7 @@ public class togglebit {
        Scanner sc=new Scanner(System.in);
        int n=sc.nextInt();
        int pos=sc.nextInt();
-
+      //toggle the bit at the given position
        n=n^(1<<pos);
        System.out.print(n);
         
