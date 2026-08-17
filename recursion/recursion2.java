2e4772f1df06941dda72a4982f28bea7b4e091d1
unknown <23ai009@sitmng.ac.in>
2026-08-17 17:51:16 +0530

    add the recursion2.java which prints 1 to 5 without for loop

---
+++ recursion/recursion2.java
@@ -0,0 +1,21 @@
+package recursion;
+
+
+public class recursion2 {
+
+  public static void printNumber(int n)
+  {
+    if(n==6)
+    {
+      return;
+    }
+    System.out.print(n+" ");
+    printNumber(n+1);
+
+  }
+    public static void main(String[] args) {
+      int n=1;
+      printNumber(n); //n=1
+
+    }
+}
