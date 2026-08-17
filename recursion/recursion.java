1de74cbf4a18fdf6bf1b30b5451205be9a982a4d
unknown <23ai009@sitmng.ac.in>
2026-08-17 14:08:17 +0530

    add the recursion1.java

---
+++ recursion/recursion.java
@@ -0,0 +1,35 @@
+package recursion;
+
+
+/*   Traditional method print the array values */
+// public class recursion {
+//     public static void main(String[] args) {
+//         int n=5;
+//         for(int i=n;i>=1;i--)
+//         {
+//             System.out.print(i+" ");
+//         }
+//     }
+// }
+
+/*   Recursion method print the array values */
+
+
+      public class recursion {
+
+        public static void printNumber(int n) {
+            if (n == 0) {
+                return;
+            } else {
+                System.out.print(n + " ");
+                printNumber(n - 1);
+            }
+        }
+    
+
+    public static void main(String[] args) {
+        int n = 5;
+        printNumber(n);
+    }
+}
+
