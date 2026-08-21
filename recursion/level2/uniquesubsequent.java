d3f924d53ccc9b3c7c705687adbdbb4287b8a417
unknown <23ai009@sitmng.ac.in>
2026-08-21 23:46:15 +0530

    fix the spacing issue in the uniquesubsequence.java

--- recursion/level2/uniquesubsequent.java
+++ recursion/level2/uniquesubsequent.java
@@ -2,36 +2,36 @@ package level2;
 
 import java.util.HashSet;
 
-public class uniquesubsequent{
+public class uniquesubsequent {
 
-    public static void subsequences(String str, int idx, String newString,HashSet<String> set) {
+    public static void subsequences(String str, int idx, String newString, HashSet<String> set) {
 
         if (idx == str.length()) {
-           if(set.contains(newString))
-           {
-            return;
-           }
-           else
-           {
-            System.out.println(newString);
-            set.add(newString);
-            return;
-           }
+            //if the newString is already present in the set then return else print the newString and add it to the set
+            if (set.contains(newString)) {
+                return;
+            } else {
+                //print the newString and add it to the set
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
         }
-
+        //get the current character
         char currentChar = str.charAt(idx);
 
         //two choices =choose or not choose
         //to be 
-        subsequences(str, idx + 1, newString + currentChar,set);
- 
+        subsequences(str, idx + 1, newString + currentChar, set);
+
         //not to be 
-        subsequences(str, idx + 1, newString,set);
+        subsequences(str, idx + 1, newString, set);
     }
 
     public static void main(String args[]) {
+        //test case for unique subsequent
         String str = "aaa";
-        HashSet<String> set=new HashSet<>();
-        subsequences(str, 0, "",set);
+        HashSet<String> set = new HashSet<>();
+        subsequences(str, 0, "", set);
     }
 }
