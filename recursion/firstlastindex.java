d17f41a269dc08ee97ec75a37b76727eac3dc4e9
unknown <23ai009@sitmng.ac.in>
2026-08-21 19:33:20 +0530

    fix the spacing and doc to the firstlastindex.java

--- recursion/firstlastindex.java
+++ recursion/firstlastindex.java
@@ -1,38 +1,36 @@
+
 public class firstlastindex {
-    static int first=-1;
-    static int last=-1;
 
-    public static void firstLastIndex(String str,char ele,int index)
+    static int first = -1;
+    static int last = -1;
 
-    {
-     if(index==str.length())
-     {
-        return;
-     }
-
-     if(str.charAt(index)==ele){
-
-        if(first==-1){
-            first=index;
+    public static void firstLastIndex(String str, char ele, int index) {
+        //base case index is equal to the length of the string then return
+        if (index == str.length()) {
+            return;
         }
-        else
-        {
-            last=index;
-        }
-     }
+        //if first element is found then store the index in first and if last element is found then store the index in last
+        if (str.charAt(index) == ele) {
 
-     firstLastIndex(str,ele,index+1);
+            if (first == -1) {
+                first = index;
+            } else {
+                last = index;
+            }
+        }
+        //recursive call for firstLastIndex
+        firstLastIndex(str, ele, index + 1);
     }
-    public static void main(String args[])
-    {
-        String str="abcaacd";
-        char ele='a';
-        firstLastIndex(str,ele,0);
+
+    public static void main(String args[]) {
+        String str = "abcaacd";
+        char ele = 'a';
+        //function call for firstLastIndex
+        firstLastIndex(str, ele, 0);
         // System.out.println("First index: " + str.charAt(first));
         // System.out.println("Last index: " + str.charAt(last));
         System.out.println("First index: " + first);
         System.out.println("Last index: " + last);
-
 
     }
 }
