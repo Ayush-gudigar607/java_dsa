7135b98302ff4813c03453e852dca61527e105b4
unknown <23ai009@sitmng.ac.in>
2026-08-21 21:45:30 +0530

    add the sunsequent.java

---
+++ recursion/level2/subsequent.java
@@ -0,0 +1,26 @@
+package level2;
+
+public class subsequent {
+
+    public static void subsequences(String str, int idx, String newString) {
+
+        if (idx == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+
+        char currentChar = str.charAt(idx);
+
+        //two choices =choose or not choose
+        //to be
+        subsequences(str, idx + 1, newString + currentChar);
+
+        //not to be 
+        subsequences(str, idx + 1, newString);
+    }
+
+    public static void main(String args[]) {
+        String str = "abc";
+        subsequences(str, 0, "");
+    }
+}
