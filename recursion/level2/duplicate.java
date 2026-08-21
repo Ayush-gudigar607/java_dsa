366c2eac3a6f5f9ed7bee7e7372d97ff5f1e8bf8
unknown <23ai009@sitmng.ac.in>
2026-08-21 21:14:25 +0530

    add the doc and fix the spacing

--- recursion/level2/duplicate.java
+++ recursion/level2/duplicate.java
@@ -1,33 +1,32 @@
 package level2;
 
 public class duplicate {
-    public static boolean[] map=new boolean[26];
-  
-    public static void removeDuplicates(String str,int idx,String newString){
+   //make boolean array of size 26 to store the elements which are already present in the string
+    public static boolean[] map = new boolean[26];
 
-        if(idx==str.length())
-        {
+    public static void removeDuplicates(String str, int idx, String newString) {
+        //if the index is equal to the length of the string then print the newString and return
+        if (idx == str.length()) {
             System.out.println(newString);
             return;
         }
-        char currentChar=str.charAt(idx);
+
+        //get the current character
+        char currentChar = str.charAt(idx);
         //already element is there then this condition
-        if(map[currentChar-'a']==true){
-             removeDuplicates(str,idx+1,newString);
-        }
-        else
-        {
+        if (map[currentChar - 'a'] == true) {
+            removeDuplicates(str, idx + 1, newString);
+        } else {
             //if the element is new then add the element then make it true
-            newString+=currentChar;
-            map[currentChar-'a']=true;
-            removeDuplicates(str,idx+1,newString);
+            newString += currentChar;
+            map[currentChar - 'a'] = true;
+            removeDuplicates(str, idx + 1, newString);
         }
     }
 
-    public static void main(String[] args)
-    {
-      String str="abbccdd";
-      removeDuplicates(str,0,"");
+    public static void main(String[] args) {
+        String str = "abbccdd";
+        removeDuplicates(str, 0, "");
     }
 
 }
