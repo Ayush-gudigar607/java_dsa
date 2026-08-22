52dd45acf14ab99b8e10a6b5a1f3a476336c1044
unknown <23ai009@sitmng.ac.in>
2026-08-22 22:04:17 +0530

    add the doc to the getFact

--- recursion/fact.java
+++ recursion/fact.java
@@ -2,11 +2,14 @@
 public class fact {
 
     public static int getFact(int n) {
+
         if (n == 1 || n == 0) {
             return 1;
         }
+        //This is the main logic of getFact return the factorial of n by calling getFact recursively with n-1 and multiplying the result with n
         int fact = getFact(n - 1);
-        return n * fact;
+        return n * fact;     //or return n*getFact(n-1)
+
     }
 
     public static void main(String[] args) {
