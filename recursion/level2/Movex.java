80a26eab7f1cffc51ffd2e4b54a4fe3075c2efa2
unknown <23ai009@sitmng.ac.in>
2026-08-22 22:46:46 +0530

    add the doc to the Movex.java

--- recursion/level2/Movex.java
+++ recursion/level2/Movex.java
@@ -1,36 +1,74 @@
 package level2;
 
-public class Movex{
-    public static void MoveX(String str,int idx,int count,String newString)
-    {
+/**
+ * This program moves all 'x' characters to the end of a string
+ * using recursion.
+ *
+ * Example:
+ * Input  : "axbcxxd"
+ * Output : "abcdxxx"
+ *
+ * Logic:
+ * - If the current character is 'x', increase the count.
+ * - If the current character is not 'x', add it to newString.
+ * - Continue recursively until the end of the string.
+ * - At the base case, add all counted 'x' characters to newString.
+ */
+public class Movex {
 
-        if(idx==str.length())
-        {
-            for(int i=0;i<count;i++)
-            {
-                newString+='x';
+    /**
+     * Moves all 'x' characters to the end of the string using recursion.
+     *
+     * @param str       input string
+     * @param idx       current index of the string
+     * @param count     number of 'x' characters found so far
+     * @param newString string containing all non-'x' characters
+     */
+    public static void MoveX(String str, int idx, int count, String newString) {
+
+        // Base case: reached the end of the string
+        if (idx == str.length()) {
+
+            // Add all the counted 'x' characters at the end
+            for (int i = 0; i < count; i++) {
+                newString += 'x';
             }
+
+            // Print the final string
             System.out.println(newString);
             return;
         }
-        char currentChar=str.charAt(idx);
 
-        if(currentChar=='x')
-            {
-                count++;
-                MoveX(str,idx+1,count,newString);
-            }
-            else
-            {
-                newString+=currentChar;
-                MoveX(str,idx+1,count,newString);
-            }
-        
+        // Get the current character
+        char currentChar = str.charAt(idx);
+
+        // If current character is 'x'
+        if (currentChar == 'x') {
+
+            // Increase the count of 'x'
+            count++;
+
+            // Move to the next character
+            MoveX(str, idx + 1, count, newString);
+
+        } else {
+
+            // Add the non-'x' character to newString
+            newString += currentChar;
+
+            // Move to the next character
+            MoveX(str, idx + 1, count, newString);
+        }
     }
 
-    public static void main(String[] args)
-    {
-  String str="axbcxxd";
-  MoveX(str,0,0,"");
+    public static void main(String[] args) {
+
+        // Input string
+        String str = "axbcxxd";
+
+        // Start recursion from index 0
+        // count = 0 because no 'x' has been found yet
+        // newString = "" because it is initially empty
+        MoveX(str, 0, 0, "");
     }
 }
