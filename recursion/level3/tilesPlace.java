a8b4d6650ec0ea226f1caef4485aca034a3d5beb
unknown <23ai009@sitmng.ac.in>
2026-08-23 21:10:29 +0530

    add the tilePlace.java

---
+++ recursion/level3/tilesPlace.java
@@ -0,0 +1,31 @@
+package level3;
+
+public class tilesPlace {
+    public static int PlaceTiles(int n,int m)
+    {
+
+        if(n==m)
+        {
+            return 2; //there are mainly 2 ways 
+        }
+
+        if(n<m)
+        {
+            return 1;  //there are mainly 1 ways it can be placed horizontally
+        }
+        //vertical placement
+        int verticalPlacement=PlaceTiles(n-m,m);
+
+        //horizontal placement
+        int horizontalPlacement=PlaceTiles(n-1,m);
+
+        return verticalPlacement+horizontalPlacement;
+
+    }
+
+    public static void main(String[] args) {
+        int n=4,m=2;
+        int NoOfWays=PlaceTiles(n, m);
+        System.out.println(NoOfWays);
+    }
+}
