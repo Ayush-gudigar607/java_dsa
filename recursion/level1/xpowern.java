ba49e51b1320b3b520c51676c2f22757a591d866
unknown <23ai009@sitmng.ac.in>
2026-08-24 20:24:15 +0530

    add the another method which reduces the dependencies on the variables

--- recursion/level1/xpowern.java
+++ recursion/level1/xpowern.java
@@ -11,13 +11,15 @@ public class xpowern {
             return 0;
         }
         
-        int xPownm1 = calPower(x, n - 1);
-        int xPown = x * xPownm1;
-        return xPown;
+        // int xPownm1 = calPower(x, n - 1);
+        // int xPown = x * xPownm1;
+        // return xPown;
+
+        return x*calPower(x, n-1);
     }
 
-    public static void main(String[] args) {
-        int x = 2, n = 5;
+     public static void main(String[] args) {
+        int x = 2, n = 2;
         int ans = calPower(x, n);
         System.out.println(ans);
     }
