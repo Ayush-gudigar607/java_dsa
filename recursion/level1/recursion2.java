e8b8bc5422a37c9daedd4cab4ee5b716cc211c19
unknown <23ai009@sitmng.ac.in>
2026-08-24 20:02:56 +0530

    add the code of recursion2 use extra variable named as i to traverse and increment it along with print the every i index value till it will be reaches to the last n value

--- recursion/level1/recursion2.java
+++ recursion/level1/recursion2.java
@@ -1,19 +1,41 @@
+
+// public class recursion2 {
+
+//     public static void printNumber(int n) {
+//         if (n == 6) {
+//             return;
+//         }
+//         System.out.print(n + " ");
+//         printNumber(n + 1);
+
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 1;
+//         printNumber(n); //n=1
+
+//     }
+// }
+
 package level1;
 
-public class recursion2 {
-
-    public static void printNumber(int n) {
-        if (n == 6) {
-            return;
-        }
-        System.out.print(n + " ");
-        printNumber(n + 1);
-
+public class recursion2{
+  public static void printNumber(int i,int n)
+  {
+    if(i==n)
+    { System.out.print(i);
+      return;
     }
 
-    public static void main(String[] args) {
-        int n = 1;
-        printNumber(n); //n=1
+    System.out.print(i+" ");
+    printNumber(i+1,n);
+  }
+  
 
-    }
+  public static void main(String[] args)
+  {
+    printNumber(1,6);
+  }
 }
+
+
