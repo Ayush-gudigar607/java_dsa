5bf1aa1a82c655dfb929382c10a35e669d4501e8
unknown <23ai009@sitmng.ac.in>
2026-08-24 19:57:42 +0530

    add the recursion.java code

--- recursion/level1/fact.java
+++ recursion/level1/fact.java
@@ -6,6 +6,7 @@ public class fact {
         if (n == 1 || n == 0) {
             return 1;
         }
+        
         //This is the main logic of getFact return the factorial of n by calling getFact recursively with n-1 and multiplying the result with n
         int fact = getFact(n - 1);
         return n * fact;     //or return n*getFact(n-1)
--- recursion/level1/recursion.java
+++ recursion/level1/recursion.java
@@ -18,11 +18,17 @@ public class recursion {
     public static void printNumber(int n) {
         if (n == 0) {
             return;
-        } else {
-            System.out.print(n + " ");
-            //recursive call to printNumber with n-1
-            printNumber(n - 1);
         }
+        // else {
+        //     System.out.print(n + " ");
+        //     //recursive call to printNumber with n-1
+        //     printNumber(n - 1);
+        // }
+
+        System.out.print(n + " ");
+        //     //recursive call to printNumber with n-1
+        printNumber(n - 1);
+
     }
 
     public static void main(String[] args) {
