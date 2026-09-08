d3e0a56d10fdb8686e6325bed09bc59564453a2d
unknown <23ai009@sitmng.ac.in>
2026-09-08 05:52:30 +0530

    add the another method for vocons to continuously check the vowel and consonants

--- BASIC CONCEPTS/CONDITIONAL STATEMENT PROBLEMS/intermidiate/vocons.java
+++ BASIC CONCEPTS/CONDITIONAL STATEMENT PROBLEMS/intermidiate/vocons.java
@@ -34,4 +34,36 @@ public class vocons {
     }
 }
 
+//Another method to continuously check the character is vowel or consonants and by using exit command exit from the program
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+       
+//        while(true)
+//        {
+//        Scanner sc=new Scanner(System.in);
+//        String ch=sc.next();
+        
+//        if(ch.equalsIgnoreCase("exit"))
+//        {
+//         break;
+//        }
+//          // Fixed: Used double quotes for strings and handled uppercase vowels
+//            if (ch.equalsIgnoreCase("a") || ch.equalsIgnoreCase("e") || 
+//                ch.equalsIgnoreCase("i") || ch.equalsIgnoreCase("o") || 
+//                ch.equalsIgnoreCase("u")) {
+//                System.out.println(ch + " is a vowel");
+//            } 
+
+
+//        else{
+//         System.out.println(ch+"is consonants");
+//        }
+//        }
+       
+//     }
+// }
+
+
 
